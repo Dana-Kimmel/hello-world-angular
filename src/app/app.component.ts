73b32bbabcbc8bcbdf53330dff70ec19fabd44d5
Dana-Kimmel <dear.prudence34@gmail.com>
2021-01-06 18:50:03 -0500

    two-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: "<h1>Hello, world!</h1>",
-  styles: ["h1 { color: red;}"]
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Hello, World!';
